2385c7079cfa0efe405111b5670d5bc34e23a2ce
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-03-15 11:53:51 +0330

    Finish the Login page

--- nav.php
+++ nav.php
@@ -13,7 +13,7 @@
                 <li class="nav-item">
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="/login.php">Login</a>
                 </li>
             </ul>
         </div>
