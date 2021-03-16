a45c0b43aaf354998a63f7804df68e82857cd0c3
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-03-16 18:35:55 +0330

    Test

--- admin/index.php
+++ admin/index.php
@@ -42,21 +42,6 @@
             </nav>
 
             <div class="container-fluid text-center">
-                <h1>Dashboard</h1>
-                <table class="table table-dark table-striped w-25 m-auto text-start">
-                    <tr>
-                        <th>Total Posts:</th>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <th>Total Users:</th>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <th>Total Views:</th>
-                        <td>100</td>
-                    </tr>
-                </table>
 
             </div>
         </div>
