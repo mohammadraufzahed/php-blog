fbeac19a8385a58ca567bfa0af8a535ccb16a985
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-03-17 12:59:48 +0330

    Settings page

---
+++ admin/pages/settings.php
@@ -0,0 +1,15 @@
+<form class="w-50 m-auto mt-5" action="#" method="POST">
+    <div class="mb-3">
+        <label for="blogName" class="form-label">Blog name</label>
+        <input type="text" class="form-control" id="blogName" placeholder="Blog Name" name="blogName">
+    </div>
+    <div class="mb-3">
+        <label for="authorName" class="form-label">Author name</label>
+        <input type="text" class="form-control" id="authorName" placeholder="Jhone Doe" name="authorName">
+    </div>
+    <div class="mb-3">
+        <label for="authorInfo" class="form-label">Author info</label>
+        <textarea class="form-control" id="authorInfo" rows="3" name="authorInfo"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Save</button>
+</form>
